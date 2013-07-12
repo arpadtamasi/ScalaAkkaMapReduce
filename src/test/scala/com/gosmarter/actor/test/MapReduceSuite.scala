861156941bbe463f5ad549470b68474dc8794aee
Árpád Tamási <rp@progos.hu>
2013-07-12 17:03:28 +0200

    cleaned up

--- src/test/scala/com/gosmarter/actor/test/MapReduceSuite.scala
+++ src/test/scala/com/gosmarter/actor/test/MapReduceSuite.scala
@@ -9,14 +9,8 @@ import org.scalatest.matchers.MustMatchers
 import org.scalatest.BeforeAndAfterAll
 import akka.testkit.ImplicitSender
 import akka.routing.RoundRobinRouter
-import com.gosmarter.client.ControllerActor
-import com.gosmarter.client.AggregateActor
-import com.gosmarter.client.ReduceActor
-import com.gosmarter.client.MapActor
-import com.gosmarter.client.Result
+import com.gosmarter.client._
 import scala.collection.mutable.Map
-import com.gosmarter.client.FileReadActor
-import com.gosmarter.client.LineReadActor
 
 class MapReduceSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
   with WordSpec with MustMatchers with BeforeAndAfterAll {
@@ -29,22 +23,22 @@ class MapReduceSpec(_system: ActorSystem) extends TestKit(_system) with Implicit
 
   "Aggregrate actor" must {
 
-    "send back Map message" in {
+    "send back AggregatedResults message" in {
 
       // create the aggregate Actor
       val aggregateActor = system.actorOf(Props[AggregateActor]);
 
-      var map: Map[String, Int] = Map[String, Int]("ak" -> 1, "kp" -> 2)
+      var map = AggregatedResults(Map[String, Int]("ak" -> 1, "kp" -> 2))
 
       aggregateActor ! map
 
-      var map1: Map[String, Int] = Map[String, Int]("ak" -> 1, "kp" -> 2)
+      var map1 = AggregatedResults(Map[String, Int]("ak" -> 1, "kp" -> 2))
       aggregateActor ! map1
 
       Thread.sleep(1000)
 
-      var output = Map("kp" -> 4, "ak" -> 2)
-      aggregateActor ! "DISPLAY_LIST"
+      var output = AggregatedResults(Map("kp" -> 4, "ak" -> 2))
+      aggregateActor ! DisplayList()
 
       expectMsg(output)
     }
@@ -52,7 +46,7 @@ class MapReduceSpec(_system: ActorSystem) extends TestKit(_system) with Implicit
 
   "Reduce actor" must {
 
-    "send back Map message" in {
+    "send back AggregatedResults message" in {
 
       // create the aggregate Actor
       val aggregateActor = system.actorOf(Props[AggregateActor]);
@@ -60,18 +54,18 @@ class MapReduceSpec(_system: ActorSystem) extends TestKit(_system) with Implicit
       // create the list of reduce Actors
       val reduceRouter = system.actorOf(Props(new ReduceActor(aggregateActor)))
 
-      val list: List[Result] = List[Result](new Result("kp", 1), new Result("ak", 2))
+      val list = Results(List[Result](Result("kp", 1), Result("ak", 2)))
 
       reduceRouter ! list
 
-      val list1: List[Result] = List[Result](new Result("ak", 1), new Result("kp", 2))
+      val list1 = Results(List[Result](Result("ak", 1), Result("kp", 2)))
 
       reduceRouter ! list1
 
       Thread.sleep(1000)
 
-      var output = Map("kp" -> 3, "ak" -> 3)
-      aggregateActor ! "DISPLAY_LIST"
+      var output = AggregatedResults(Map("kp" -> 3, "ak" -> 3))
+      aggregateActor ! DisplayList()
 
       expectMsg(output)
     }
@@ -79,7 +73,7 @@ class MapReduceSpec(_system: ActorSystem) extends TestKit(_system) with Implicit
 
   "Map actor" must {
 
-    "send back Map message" in {
+    "send back AggregatedResults message" in {
 
       // create the aggregate Actor
       val aggregateActor = system.actorOf(Props[AggregateActor]);
@@ -90,21 +84,21 @@ class MapReduceSpec(_system: ActorSystem) extends TestKit(_system) with Implicit
       // create the list of map Actors
       val mapRouter = system.actorOf(Props(new MapActor(reduceRouter)).withRouter(RoundRobinRouter(nrOfInstances = 2)))
 
-      var line = "Aditya Krishna Kartik Manjula"
+      var line = Line("Aditya Krishna Kartik Manjula")
 
       mapRouter ! line
 
       Thread.sleep(1000)
 
-      var output = Map("Kartik" -> 1, "Krishna" -> 1, "Aditya" -> 1, "Manjula" -> 1)
-      aggregateActor ! "DISPLAY_LIST"
+      var output = AggregatedResults(Map("Kartik" -> 1, "Krishna" -> 1, "Aditya" -> 1, "Manjula" -> 1))
+      aggregateActor ! DisplayList()
       expectMsg(output)
     }
   }
 
   "List Reader Controller actor" must {
 
-    "send back Map message" in {
+    "send back AggregatedResults message" in {
 
       // create the aggregate Actor
       val aggregateActor = system.actorOf(Props[AggregateActor]);
@@ -119,21 +113,20 @@ class MapReduceSpec(_system: ActorSystem) extends TestKit(_system) with Implicit
 
       val lineReadActor = system.actorOf(Props[LineReadActor])
 
-      var list = List[String]("Aditya Krishna Kartik Manjula", "Manjula Anand Aditya Kartik", "Anand Vani Phani Aditya", "Kartik Krishna Manjula Aditya", "Vani Phani Anand Manjula")
-
+      var list = Lines(List[String]("Aditya Krishna Kartik Manjula", "Manjula Anand Aditya Kartik", "Anand Vani Phani Aditya", "Kartik Krishna Manjula Aditya", "Vani Phani Anand Manjula"))
       lineReadActor.tell(list, controller)
 
       Thread.sleep(1000)
 
-      var output = Map("Anand" -> 3, "Kartik" -> 3, "EOF" -> 1, "Krishna" -> 2, "Vani" -> 2, "Phani" -> 2, "Aditya" -> 4, "Manjula" -> 4)
-      aggregateActor ! "DISPLAY_LIST"
+      var output = AggregatedResults(Map("Anand" -> 3, "Kartik" -> 3, "Krishna" -> 2, "Vani" -> 2, "Phani" -> 2, "Aditya" -> 4, "Manjula" -> 4))
+      aggregateActor ! DisplayList()
       expectMsg(output)
     }
   }
 
   "File Reader Controller actor" must {
 
-    "send back DISPLAYED message" in {
+    "send back Displayed message" in {
 
       // create the aggregate Actor
       val aggregateActor = system.actorOf(Props[AggregateActor]);
@@ -148,12 +141,12 @@ class MapReduceSpec(_system: ActorSystem) extends TestKit(_system) with Implicit
 
       val fileReadActor = system.actorOf(Props[FileReadActor])
 
-      fileReadActor.tell("/Othello.txt", controller)
+      fileReadActor.tell(File("/Othello.txt"), controller)
 
       Thread.sleep(5000)
 
-      controller ! "DISPLAY_LIST"
-      expectMsg("DISPLAYED")
+      controller ! DisplayList()
+      expectMsg(Displayed())
     }
   }
 }
